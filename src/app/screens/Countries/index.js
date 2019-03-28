a0a3ee238a84d15048256ce1f34b6287de44ae1c
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-03-28 20:25:15 -0300

    Addded countries view

---
+++ src/app/screens/Countries/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class Countries extends Component {
+  render() {
+    return (
+    <div className="main-container">
+      <h1 className="countries-title">Paises</h1>
+      <div className="countries-container">
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Countries;

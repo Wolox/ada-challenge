7aa213988828ce80c89dcb3b990365ea95098840
Oriana Colmenares <oriananais@gamil.com>
2019-04-02 17:39:37 -0300

    terminado punto 2, componente que muestra distintos countries

--- src/app/screens/Countries/index.js
+++ src/app/screens/Countries/index.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import listOfCountries from "../../constants/countries";
+import "./styles.css";
 
-import './styles.css';
+class Country extends Component {
+  state = {};
 
-class Countries extends Component {
   render() {
     return (
-    <main className="main-container">
-      <h1 className="countries-title">Paises</h1>
-      <ul className="countries-container">
-        {
-          // Agregar los países aqui
-        }
-      </ul>
-    </main>
+      <li>
+        {this.props.name}. Poblacion: {this.props.poblacion}. Region:{" "}
+        {this.props.region}
+      </li>
+    );
+  }
+}
+
+//export default Country;
+
+class Countries extends Component {
+  state = {
+    countriesList: listOfCountries
+  };
+
+  renderCountries = () => {
+    let countries = [];
+    for (let i = 0; i < 8; i++) {
+      const country = (
+        <Country
+          name={this.state.countriesList[i].name}
+          poblacion={this.state.countriesList[i].population}
+          region={this.state.countriesList[i].region}
+          flag={this.state.countriesList[i].flag}
+        />
+      );
+      countries.push(country);
+    }
+    return countries;
+  };
+
+  render() {
+    console.log(this.state.countriesList);
+    return (
+      <main className="main-container">
+        <h1 className="countries-title">Paises</h1>
+        <ul className="countries-container">
+          {this.renderCountries()}
+        </ul>
+      </main>
     );
   }
 }

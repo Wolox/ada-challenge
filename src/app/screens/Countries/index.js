f9b185942a95f41052207dc187ebc2e41c74c7e7
Zoe <zoeyusti@gmail.com>
2019-04-09 22:30:39 -0300

    trajimos los paises y pusimos clases

--- src/app/screens/Countries/index.js
+++ src/app/screens/Countries/index.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 
 import './styles.css';
 
+import countries from '../../constants/countries.js';
+
 class Countries extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      //paises:[]
+      countries: countries
+    }
+
+  }
+
+
   render() {
+
+    const paises = this.state.countries.map(function(pais){
+
+      return (
+        <div className="country">
+          <img src={pais.flag} className="country-flag" alt="bandera"/>
+          <h5 className="country-name">{pais.name}</h5> 
+          <div className="country-info-wrapper">
+            <p className="country-info-region">{pais.region}</p> 
+            <p className="country-info-population">{pais.population}</p>
+          </div>
+        </div>);
+    });
+
     return (
-    <main className="main-container">
-      <h1 className="countries-title">Paises</h1>
-      <ul className="countries-container">
-        {
-          // Agregar los países aqui
-        }
-      </ul>
-    </main>
+      <main className="main-container">
+        <h1 className="countries-title">Paises</h1>
+        <ul className="countries-container">
+          {
+            // Agregar los países aqui
+            <li>{paises}</li>              
+            
+          }
+        </ul>
+      </main>
     );
   }
 }

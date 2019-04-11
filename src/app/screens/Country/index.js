b0f398efe36c2eb810a0fe4e47b6e3ac7ed25378
flavorflap <flaviabocco@gmail.com>
2019-04-11 19:35:32 -0300

    rendering info and images from api

--- src/app/screens/Country/index.js
+++ src/app/screens/Country/index.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 
-import './styles.css';
+import './styles.scss';
 
 import argentina from '../../constants/argentina';
+import { getCountries } from '../../services/countries';
 
 class Country extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      countryName:'',
+      countryCapital:'',
+      countryLanguage:'',
+      countryCurrency:'',
+      countryPopulation:'',
+      countryTimeZone:'',
+      countryImages:[]
+    }
+  }
+  componentDidMount(){
+    getCountries().then(country => {
+      this.setState({
+        countryName: country.country_name,
+        countryCapital: country.capital_city,
+        countryLanguage: country.national_language,
+        countryCurrency: country.currency,
+        countryPopulation: country.population,
+        countryTimeZone: country.time_zone,
+        countryImages: country.images
+      })
+    })
+  }
   render() {
+    console.log(this.state.countryImages)
+    const images = this.state.countryImages.map (i => <img className="country-image" src={i}/>)
     return (
 
     <div className="main-container">
-      <h1 className="country-title">Nombre del pais desde constante o api</h1>
+      <h1 className="country-title">{this.state.countryName}</h1>
       <div className="country-container">
         <div className="images-container">
-        {/*  Acá van las 4 imágenes */}
+        {images}
         </div>
         <ul className="country-info">
-        {/*  Acá va la información del país */}
+          <li><span className ="country-info-label">Capital:</span> {this.state.countryCapital}</li>
+          <li><span className ="country-info-label">National Language:</span> {this.state.countryLanguage}</li>
+          <li><span className ="country-info-label">Currency:</span> {this.state.countryCurrency}</li>
+          <li><span className ="country-info-label">Population:</span> {this.state.countryPopulation}</li>
+          <li><span className ="country-info-label">Time Zone:</span> {this.state.countryTimeZone}</li>
         </ul>
       </div>
     </div>
